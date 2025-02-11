f20636878a2a3e3e966daabae511c2243211d7da
Mark Connell <github@markconnell.co.uk>
2025-02-11 09:23:10 +0000

    play nice with case-sensitive file systems

--- lib/was.rb
+++ lib/was.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-require_relative "WAS/version"
-require_relative "WAS/score"
+require_relative "was/version"
+require_relative "was/score"
 
 module WAS
   class Error < StandardError; end
