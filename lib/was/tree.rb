9e5f4867ff72293c6fa97d312695cca32882f9e9
Mark Connell <github@markconnell.co.uk>
2025-02-26 15:57:05 +0000

    start ordering tree.. failing complex spec

--- lib/was/tree.rb
+++ lib/was/tree.rb
@@ -1,4 +1,25 @@
 module WAS
   class Tree < Hash
+    def order(order_key = :deduction)
+      self.dup.tap do |tree|
+        return tree if tree[:with].nil?
+
+        if order_key == :deduction
+          tree[:with] = sorted_with_by(order_key, tree).to_h
+        else
+          tree[:with] = sorted_with_by(order_key, tree).reverse.to_h
+        end
+      end
+    end
+
+    private
+
+    def sorted_with_by(order_key, tree)
+      return tree if tree[:with].nil?
+
+      tree[:with].sort_by do |_, subtree|
+        subtree[order_key]
+      end
+    end
   end
 end
--- spec/was/tree_spec.rb
+++ spec/was/tree_spec.rb
@@ -4,4 +4,93 @@ RSpec.describe WAS::Tree do
   it "is a subclass of Hash" do
     expect(described_class.ancestors).to include(Hash)
   end
+
+  let(:report_tree) do
+    WAS::Tree.new.tap do |tree|
+      tree[:score]     = 750
+      tree[:max]       = 1000
+      tree[:deduction] = -250
+      tree[:weight]    = 1.0,
+      tree[:with]      = {
+        smaller: small_tree,
+        bigger: big_tree
+      }
+    end
+  end
+
+  describe "#order" do
+    context "ordering by :deduction" do
+      let(:small_tree) do
+        WAS::Tree.new.tap do |tree|
+          tree[:score]     = 200
+          tree[:max]       = 250
+          tree[:deduction] = -50
+          tree[:weight]    = 0.5
+        end
+      end
+
+      let(:big_tree) do
+        WAS::Tree.new.tap do |tree|
+          tree[:score]     = 50
+          tree[:max]       = 250
+          tree[:deduction] = -200
+          tree[:weight]    = 0.5
+        end
+      end
+
+      it "returns a tree ordered by largest deductions" do
+        expect(report_tree[:with].keys).to eq([:smaller, :bigger])
+        expect(report_tree.order[:with].keys).to(
+          eq([:bigger, :smaller])
+        )
+      end
+    end
+
+    context "ordering by :max" do
+      let(:small_tree) do
+        WAS::Tree.new.tap do |tree|
+          tree[:score]     = 50
+          tree[:max]       = 50
+          tree[:deduction] = 0
+          tree[:weight]    = 0.5
+        end
+      end
+
+      let(:medium_tree) do
+        WAS::Tree.new.tap do |tree|
+          tree[:score]     = 150
+          tree[:max]       = 150
+          tree[:deduction] = 0
+          tree[:weight]    = 0.5
+        end
+      end
+
+      let(:big_tree) do
+        WAS::Tree.new.tap do |tree|
+          tree[:score]     = 100
+          tree[:max]       = 100
+          tree[:deduction] = 0
+          tree[:weight]    = 0.5
+          tree[:with]      = {
+            smaller: small_tree,
+            medium: medium_tree
+          }
+        end
+      end
+
+      it "returns a tree ordered by largest max values" do
+        expect(report_tree[:with].keys).to eq([:smaller, :bigger])
+
+        result = report_tree.order(:max)
+
+        expect(result[:with].keys).to(
+          eq([:bigger, :smaller])
+        )
+
+        expect(result[:with][:bigger][:with].keys).to(
+          eq([:medium, :smaller])
+        )
+      end
+    end
+  end
 end
